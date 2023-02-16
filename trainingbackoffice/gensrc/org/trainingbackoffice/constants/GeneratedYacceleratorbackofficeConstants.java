9271582abbdce625705b4645b875ddddec16ff74
aleynahasagdas <aleyna.hasagdas@boun.edu.tr>
2023-02-16 03:32:55 +0300

    Initial commit

---
+++ trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Feb 16, 2023, 2:27:22 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
